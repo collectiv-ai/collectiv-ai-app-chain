a563d06646e12cc83ee76affd828e343ada64f55
CollectiVAI <info@collectivai.org>
2025-11-29 11:54:01 +0100

    Create module.go

---
+++ x/collectivai/module.go
@@ -0,0 +1,11 @@
+package collectivai
+
+// Module is a placeholder for the future CollectiVAI module.
+//
+// This module is intended to encapsulate:
+// - Civic / governance specific state & logic
+// - Custom messages, queries and parameters
+// - Integration with the core app (Cosmos SDK modules)
+type Module struct {
+	// TODO: define keeper, codec, and other wiring once the module is implemented
+}
