8625316c40cf4b8e39d3c5cec89b447dab05732b
CollectiVAI <info@collectivai.org>
2025-11-29 11:53:35 +0100

    Create app.go

---
+++ app/app.go
@@ -0,0 +1,11 @@
+package app
+
+// App is a placeholder for the future Cosmos SDK application.
+//
+// Later this will hold:
+// - Module wiring (bank, staking, gov, custom collectivai module)
+// - Encoding / interfaces
+// - App initialization & configuration
+type App struct {
+	// TODO: add fields once the Cosmos SDK integration starts
+}
